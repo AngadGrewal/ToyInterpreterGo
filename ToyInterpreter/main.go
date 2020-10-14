375608d3c093394b1dbd15e99525897d4bc8fa45
Maheep <mag922@cs.bham.ac.uk>
2020-10-15 00:53:25 +0100

    -- read file line by line --

--- ToyInterpreter/main.go
+++ ToyInterpreter/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+// bufio is used to read a file line by line this implements buffered
+// io. Wraps an io.Reader or io.Writer Object, creating another object
+// that also implements the interface but provides buffering and some
+// help for textual I/O.
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
-	fmt.Println("")
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
